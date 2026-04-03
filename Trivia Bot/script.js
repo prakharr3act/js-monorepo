216453f86e52a1d9c7b76a97e523665bc82412ce
Prakhar Patel <prakhar.codes433@gmail.com>
2026-04-03 10:45:04 +0530

    Add files via upload

---
+++ Trivia Bot/script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Lexi";
+let botLocation = "the Cloud";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in only 10 days!";
+console.log(codingFact);
+
+codingFact = "The first version of " + favoriteLanguage + " was actually called Mocha.";
+console.log(codingFact);
+
+codingFact = "Almost every website you visit uses " + favoriteLanguage + " to be interactive.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
